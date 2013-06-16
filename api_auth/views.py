3e1e930c8fa6f84fe5793785bcaf2bef825938d5
Chris Ridenour <chrisridenour@gmail.com>
2013-06-15 23:49:32 -0400

    Trying to replicate django rest framework and so far failing

--- api_auth/views.py
+++ api_auth/views.py
@@ -13,7 +13,116 @@ import json
 logger = logging.getLogger(settings.TOKEN_LOGGER_NAME)
 
 
-class ProtectedView(View):
+class APIView(View):
+    """
+    API View
+
+     * Offers functions for returning JSON objects
+     * Catches api_auth.exceptions and returns intelligent HTTP status code
+     * Pareses request body to JSON object if one exists
+     * Defaults to only allowing OPTIONS HTTP method
+    """
+    http_method_names = ['options', ]
+
+    def _parse_body(self, request):
+        """
+        Internal function to parse out the post data.
+        """
+        return json.loads(request.body)
+
+    def json_response(self, request, *args, **kwargs):
+        """
+        Calls internal functions and returns the expected 2xx status or fails with another status
+        """
+
+        # What is our expected HTTP Status Code on success?
+        success_status = kwargs.get('status_code', 200)
+
+        # Does this request have a payload?
+        has_payload = request.body is not None
+
+        # Initialize our data variable
+        data = {}
+
+        if has_payload:
+            try:
+                data = self._parse_body(request)
+            except TypeError as e:
+                logger.exception('Error decoding JSON', e.args)
+                return HttpResponse(status=400)
+
+        try:
+            if has_payload:
+                response = self.parse_data(request, data, *args, **kwargs)
+            else:
+                response = self.get_data(request, *args, **kwargs)
+
+            if type(response) is dict or type(response) is list:
+                return HttpResponse(
+                    status=success_status,
+                    content_type='application/json',
+                    content=json.dumps(response)
+                )
+            elif response:
+                return HttpResponse(status=success_status)
+
+        except AccessDenied as e:
+            logger.info('Access denied to resource.', e.args)
+            return HttpResponse(status=403)
+
+        except PageNotFound as e:
+            logger.info('Page not found.', e.args)
+            return HttpResponse(status=404)
+
+        except ServiceUnavailable as e:
+            logger.exception(e, e.args)
+            return HttpResponse(status=503)
+
+        except Exception as e:
+            logger.exception(e, e.args)
+            return HttpResponse(status=500)
+
+    def parse_data(self, request, data, *args, **kwargs):
+        """
+        Work with the data and return true or an object for 200. Throw exceptions otherwise.
+        """
+        raise NotImplementedError('%s methods require a parse_data function.' % request.method)
+
+    def get_data(self, request, *args, **kwargs):
+        """
+        Return true or an object for 200. Throw exceptions otherwise.
+        """
+        raise NotImplementedError('%s methods require a get_data function.' % request.method)
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        """
+        Ensure CSRF tokens are not required on our calls
+        """
+        return super(APIView, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        # Set a default status code for a success - easy to override
+        kwargs.update({'status_code': 200})
+        return self.json_response(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        # Set a default status code for a success - easy to override
+        kwargs.update({'status_code': 200})
+        return self.json_response(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        # Set a default status code for a success - easy to override
+        kwargs.update({'status_code': 201})
+        return self.json_response(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        # Set a default status code for a success - easy to override
+        kwargs.update({'status_code': 200})
+        return self.json_response(request, *args, **kwargs)
+
+
+class ProtectedView(APIView):
     """
     Protected View
 
@@ -40,99 +149,3 @@ class ProtectedView(View):
         request.user = user
 
         return super(ProtectedView, self).dispatch(request, *args, **kwargs)
-
-
-class PostJSONView(View):
-    http_method_names = 'post'
-
-    def _get_data(self, request):
-        """
-        Internal function to parse out the post data.
-        """
-        return json.loads(request.raw_post_data)
-
-    def post(self, request, *args, **kwargs):
-        """
-        Method called by Django. Handles finding the data and passing to the parse_data function
-        """
-        try:
-            data = self._get_data(request)
-        except TypeError as e:
-            logger.exception('Error decoding JSON', e.args)
-            return HttpResponse(status=400)
-
-        try:
-            response = self.parse_data(request, data, *args, **kwargs)
-
-            if type(response) is dict or type(response) is list:
-                return HttpResponse(
-                    status=200,
-                    content_type='application/json',
-                    content=json.dumps(response)
-                )
-            elif response:
-                return HttpResponse(status=200)
-
-        except AccessDenied as e:
-            logger.info('Access denied to resource.', e.args)
-            return HttpResponse(status=403)
-
-        except PageNotFound as e:
-            logger.info('Page not found.', e.args)
-            return HttpResponse(status=404)
-
-        except ServiceUnavailable as e:
-            logger.exception(e, e.args)
-            return HttpResponse(status=503)
-
-        except Exception as e:
-            logger.exception(e, e.args)
-            return HttpResponse(status=500)
-
-    def parse_data(self, request, data, *args, **kwargs):
-        """
-        Work with the data and return true or an object for 200. Throw exceptions otherwise.
-        """
-        raise PageNotFound
-
-
-class GetJSONView(View):
-    http_method_names = 'get'
-
-    def get_data(self, request, *args, **kwargs):
-        """
-        Return true or an object for 200. Throw exceptions otherwise.
-        """
-        raise PageNotFound
-
-    def get(self, request, *args, **kwargs):
-        """
-        Method called by Django. Passes actual request to the get_data function
-        """
-        try:
-            response = self.get_data(request, *args, **kwargs)
-
-            if type(response) is dict or type(response) is list:
-                return HttpResponse(
-                    status=200,
-                    content_type='application/json',
-                    content=json.dumps(response)
-                )
-            elif response:
-                return HttpResponse(status=200)
-
-        except AccessDenied as e:
-            logger.info('Access denied to resource.', e.args)
-            return HttpResponse(status=403)
-
-        except PageNotFound as e:
-            logger.info('Page not found.', e.args)
-            return HttpResponse(status=404)
-
-        except ServiceUnavailable as e:
-            logger.exception(e, e.args)
-            return HttpResponse(status=503)
-
-        except Exception as e:
-            logger.exception(e, e.args)
-            return HttpResponse(status=500)
